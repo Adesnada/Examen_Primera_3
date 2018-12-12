955535cb39228dcc5df51f86d53f7e59f8756d56
Adrian <adri_97_cr@hotmail.com>
2018-12-12 21:03:46 +0100

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    float velocidad = 10f;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey("d"))
+        {
+            this.transform.Translate(velocidad * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            this.transform.Translate(-velocidad * Time.deltaTime, 0, 0);
+        }
+    }
+}
